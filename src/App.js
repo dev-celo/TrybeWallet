2aa05a47447cf377c4edc5cde43e13189aa3caac
marcelo <marcelohenrique.backend@gmail.com>
2023-02-01 18:43:45 -0300

    Requisito 02 ok - component header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 function App() {
   return (
     <div>
+      <Route path="/carteira" component={ Wallet } />
       <Route exact path="/" component={ Login } />
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,29 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
   render() {
+    const { email } = this.props;
+    console.log(email);
     return (
-      <div>Header</div>
+      <div>
+        <span data-testid="email-field">{email}</span>
+        <div>
+          <span data-testid="total-field">{`Despesa total: ${0}`}</span>
+          <span data-testid="header-currency-field">BRL</span>
+        </div>
+      </div>
     );
   }
 }
 
-export default Header;
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  ...state.user,
+});
+
+export default connect(mapStateToProps)(Header);
