5515286892ac02e15f7e2dd8e80a83be6db3fb83
marcelo <marcelohenrique.backend@gmail.com>
2023-02-02 21:59:39 -0300

    Adicionando o thead e tbody

--- src/components/Table.js
+++ src/components/Table.js
@@ -8,37 +8,41 @@ class Table extends Component {
     return (
       <div>
         <table>
-          <tr>
-            <th>Descrição</th>
-            <th>Tag</th>
-            <th>Método de pagamento</th>
-            <th>Valor</th>
-            <th>Moeda</th>
-            <th>Câmbio utilizado</th>
-            <th>Valor convertido</th>
-            <th>Moeda de conversão</th>
-            <th>Editar/Excluir</th>
-          </tr>
-          {
-            expenses
-            && expenses.map((expense, index) => (
-              <tr key={ index }>
-                <td>{ expense.description }</td>
-                <td>{ expense.tag }</td>
-                <td>{ expense.method }</td>
-                <td>{ Number(expense.value).toFixed(2) }</td>
-                <td>{ expense.exchangeRates[expense.currency].name }</td>
-                <td>
-                  { Number(expense.exchangeRates[expense.currency].ask).toFixed(2) }
-                </td>
-                <td>
-                  { Number(expense.value * expense.exchangeRates[expense.currency].ask)
-                    .toFixed(2) }
-                </td>
-                <td>Real</td>
-              </tr>
-            ))
-          }
+          <thead>
+            <tr>
+              <th>Descrição</th>
+              <th>Tag</th>
+              <th>Método de pagamento</th>
+              <th>Valor</th>
+              <th>Moeda</th>
+              <th>Câmbio utilizado</th>
+              <th>Valor convertido</th>
+              <th>Moeda de conversão</th>
+              <th>Editar/Excluir</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              expenses
+              && expenses.map((expense, index) => (
+                <tr key={ index }>
+                  <td>{ expense.description }</td>
+                  <td>{ expense.tag }</td>
+                  <td>{ expense.method }</td>
+                  <td>{ Number(expense.value).toFixed(2) }</td>
+                  <td>{ expense.exchangeRates[expense.currency].name }</td>
+                  <td>
+                    { Number(expense.exchangeRates[expense.currency].ask).toFixed(2) }
+                  </td>
+                  <td>
+                    { Number(expense.value * expense.exchangeRates[expense.currency].ask)
+                      .toFixed(2) }
+                  </td>
+                  <td>Real</td>
+                </tr>
+              ))
+            }
+          </tbody>
         </table>
       </div>
     );
